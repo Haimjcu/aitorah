e2d3caa6adca834af7a924889079891753b6b2e4
haimlubin-workiz <96918713+WorkizBizOps@users.noreply.github.com>
2026-06-21 14:56:13 +0300

    fix auth url

--- lib/auth.ts
+++ lib/auth.ts
@@ -39,6 +39,7 @@ if (process.env.GOOGLE_CLIENT_ID && process.env.GOOGLE_CLIENT_SECRET) {
 }
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
+  trustHost: true,
   adapter: hasDb
     ? DrizzleAdapter(getDb(), {
         usersTable: users,
